29b042bf8cf4aba42aad5fb42515678abe159685
roccol82 <68331367+roccol82@users.noreply.github.com>
2020-07-15 21:53:21 +0200

    Update InstallIIS.ps1

--- InstallIIS.ps1
+++ InstallIIS.ps1
@@ -1,4 +1,4 @@
 {
-  "fileUris": ["https://raw.githubusercontent.com/alashro/sampleweb/master/InstallIIS.ps1"],
+  "fileUris": ["https://raw.githubusercontent.com/roccol82/azureset/master/InstallIIS.ps1"],
   "commandToExecute": "powershell.exe ./InstallIIS.ps1"
 }
